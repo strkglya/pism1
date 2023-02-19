4f26bd8ada2ff5726ea0abcf2fb00fdead855057
strkglya <alealehandra@mail.ru>
2023-02-19 22:12:58 +0300

    Vars created

---
+++ application/src/main/java/com/github/rsoi/domain/telephoneInfo.java
@@ -0,0 +1,13 @@
+package com.github.rsoi.domain;
+
+public class telephoneInfo {
+
+    //vars
+    private String nameOfTheTelephone;
+    private Double priceOfTheTelephone;
+    private Double minimumPrice;
+    private Double maximumPrice;
+    private Double sizeOfTheScreen;
+    private Double amountOfRAM;
+    private Boolean sdCardIsAvailable;
+}
