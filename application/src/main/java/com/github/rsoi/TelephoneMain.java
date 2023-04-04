221688bc7ee5c7e1c79d709d76fae86e978acd24
strkglya <alealehandra@mail.ru>
2023-04-04 19:14:50 +0300

    lab4 fin

--- application/src/main/java/com/github/rsoi/TelephoneMain.java
+++ application/src/main/java/com/github/rsoi/TelephoneMain.java
@@ -1,22 +1,16 @@
 package com.github.rsoi;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 @RequiredArgsConstructor
-public class TelephoneMain implements CommandLineRunner {
-
+public class TelephoneMain {
     public static void main(String[] args) {
         SpringApplication.run(TelephoneMain.class, args);
     }
 
-    @Override
-    public void run(String... args) {
-
-    }
 }
 
 
